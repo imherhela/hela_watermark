a6f99b937d812e1db5f55d28cd59138fb03c5d74
ImHerHela <76200467+imherhela@users.noreply.github.com>
2024-11-22 07:25:58 -0800

    Update app.js

--- html/app.js
+++ html/app.js
@@ -1,26 +1,17 @@
+window.addEventListener('message', function (e) {
+    $("#container").stop(false, true);
 
-window.addEventListener('message', function(e) {
-	$("#container").stop(false, true);
-    if (e.data.displayWindow == 'true') {
-        $("#container").css('display', 'flex');
-  		
-        $("#container").animate({
-        	bottom: "25%",
-        	opacity: "1.0"
-        	},
-        	700, function() {
-
-        });
-
+    if (e.data.type === 'DisplayWM' && e.data.visible === true) { 
+        const position = e.data.position || 'top-right';
+        $("#container").removeClass("top-right bottom-left top-left").addClass(position);
+        $("#container").css('display', 'flex').animate({
+            opacity: "1.0"
+        }, 700);
     } else {
-    	$("#container").animate({
-        	bottom: "-50%",
-        	opacity: "0.0"
-        	},
-        	700, function() {
-        		$("#container").css('display', 'none');
-	         	
+        $("#container").animate({
+            opacity: "0.0"
+        }, 700, function () {
+            $("#container").css('display', 'none');
         });
     }
 });
-
