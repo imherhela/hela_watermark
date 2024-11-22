6ae2780c29dbf3b210e73524f23204e339a1fc49
ImHerHela <76200467+imherhela@users.noreply.github.com>
2024-11-22 07:25:22 -0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,5 @@
 config = {
-    allowoff = true -- Should the player be able to turn off the watermark?
+    framework = 'vorp', -- Change this to 'rsg' or 'redemrp' as needed    
+    allowoff = true, -- Should the player be able to turn off the watermark?
+    position = 'bottom-left' -- Default position for the watermark ('top-right', 'top-left')
 }
