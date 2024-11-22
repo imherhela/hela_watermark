199b869d195eaa2a5e55c223a83cc9d84afacd39
ImHerHela <76200467+imherhela@users.noreply.github.com>
2024-11-22 07:25:06 -0800

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,56 +1,98 @@
 -----------------------------------------------------------------
 -- Logo Watermark- A Simple FiveM Script, Made By Jordan.#2139 --
---  RedM Watermark- A simple RedM script, Edited by ImHerHela	 --
+--  hela_watermark- A simple RedM script, Edited by ImHerHela  --
 -----------------------------------------------------------------
-
-----------------------------------------------------------------------------------------------
-                  -- !WARNING! !WARNING! !WARNING! !WARNING! !WARNING! --
-        -- DO NOT TOUCH THIS FILE OR YOU /WILL/ FUCK SHIT UP! EDIT NOTHING --
--- DO NOT BE STUPID AND WHINE TO ME ABOUT THIS BEING BROKEN IF YOU TOUCHED THE LINES BELOW. --
-----------------------------------------------------------------------------------------------
-local isUiOpen = false 
+local isUiOpen = false
 local userTurnedOff = false
 
 Citizen.CreateThread(function()
-  if NetworkIsSessionStarted() then
-    TriggerEvent("chat:addSuggestion", "/watermark", "help text", {
-      { help='Toggle the watermark' }
-    })
-    return
+    local previousState = false
+
+    while true do
+        Citizen.Wait(100)
+
+        local isMapOrUiActive = IsRadarHidden() or IsPauseMenuActive()
+
+        if isMapOrUiActive ~= previousState then
+          previousState = isMapOrUiActive
+          if not userTurnedOff then
+              showWM(not isMapOrUiActive)
+          end
+      end
   end
 end)
 
-AddEventHandler('onClientMapStart', function ()
-  Citizen.CreateThread(function ()
-    isUiOpen = true
-    TriggerEvent('DisplayWM', true) 
+function showWM(display)
+  local success, err = pcall(function()
+      SendNUIMessage({
+          type = 'DisplayWM',
+          visible = display,
+          position = config.position -- Pass the position from the config
+      })
   end)
-end)
-
-RegisterNetEvent('DisplayWM', function (status)
-  if status then 
-    SendNUIMessage({displayWindow = 'true'})
-    isUiOpen = true
-    userTurnedOff = false
-  else
-    SendNUIMessage({displayWindow = 'false'})
-    isUiOpen = false
-    userTurnedOff = true
+  if not success then
+      print("^1Error sending NUI message: " .. err)
   end
+  isUiOpen = display
+end
+
+-- Trigger on session start
+Citizen.CreateThread(function()
+    if NetworkIsSessionStarted() then
+        TriggerEvent("chat:addSuggestion", "/watermark", "Toggle the watermark display")
+        return
+    end
 end)
 
+-- Event: Character selected
+if config.framework == 'vorp' then
+  RegisterNetEvent("vorp:SelectedCharacter")
+  AddEventHandler("vorp:SelectedCharacter", function(charid)
+      HandleCharacterSelection()
+  end)
+elseif config.framework == 'rsg' then
+  RegisterNetEvent("RSGCore:Client:OnPlayerLoaded")
+  AddEventHandler("RSGCore:Client:OnPlayerLoaded", function(data)
+      HandleCharacterSelection()
+  end)
+elseif config.framework == 'redemrp' then
+  RegisterNetEvent("redemrp_charselect:SpawnCharacter")
+  AddEventHandler("redemrp_charselect:SpawnCharacter", function()
+      HandleCharacterSelection()
+  end)
+else
+  print("Error: Unsupported framework specified in Config.Framework")
+end
+
+function HandleCharacterSelection()
+  Citizen.Wait(30000) -- Wait for 30 seconds
+  isUiOpen = true
+  TriggerEvent('DisplayWM', true)
+end
+
+-- Event: Display watermark
+RegisterNetEvent('DisplayWM')
+AddEventHandler('DisplayWM', function(status)
+    if status then
+        userTurnedOff = false 
+        showWM(true)
+    else
+        userTurnedOff = true
+        showWM(false)
+    end
+end)
+
+-- Command: Toggle watermark
 RegisterCommand('watermark', function()
   if config.allowoff then
-    if not isUiOpen then
-      TriggerEvent('DisplayWM', true)
-    else
-      TriggerEvent('DisplayWM', false) 
-    end
-  else 
-    TriggerEvent('chat:addMessage', {
-      color = { 255, 0, 0},
-      multiline = false,
-      args = {"^9[RedM-WM] ^1This server has disabled the option to toggle the watermark!"}
-    })    
+      local newStatus = not isUiOpen
+      TriggerEvent('DisplayWM', newStatus)
+  else
+      TriggerEvent('chat:addMessage', {
+          color = {255, 0, 0},
+          multiline = false,
+          args = {"^9[RedM-WM] ^1This server has disabled the option to toggle the watermark!"}
+      })
   end
 end)
+
