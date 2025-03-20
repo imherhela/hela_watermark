60ad13de0d85a3510aea86e7af6471667bace586
ImHerHela <76200467+imherhela@users.noreply.github.com>
2025-03-20 12:42:23 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,15 +4,13 @@
 -----------------------------------------------------------------
 
 game 'rdr3'
-fx_version 'adamant'
+fx_version 'cerulean'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 lua54 'yes'
 
 -- Define the resource metadata
-name "hela_watermark"
 description "A Simple Watermark Script"
 author "ImHerHela"
-version "v1.0"
 
 client_scripts {
 	'config.lua',
@@ -24,3 +22,12 @@ files {
 	'html/*',
 	'img/logo.png'
 }
+
+dependency {
+	'vorp_core'
+}
+
+version "1.1.0"
+vorp_checker 'yes'
+vorp_name '^5Hela_Watermark ^4Version Check^3
+vorp_github 'https://github.com/imherhela/hela_watermark'
